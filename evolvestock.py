8ad52a5d92b6035b07d0f82044fd72dd193fadfc
J S <strimp101@gmail.com>
2012-01-04 20:19:42 -0600

    Initial commit

---
+++ evolvestock.py
@@ -0,0 +1,24 @@
+"""
+Simulates and optionally plots a normally distributed Wiener Process with
+non-overlapping, independent periods
+"""
+
+import math
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import wienerprocess as wp
+
+def evolvestock():
+    """
+    Parameters
+    --------------
+
+    Returns
+    ---------
+
+    Usage
+    -------
+    
+    """
